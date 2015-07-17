fdd490002c6bc4f6d7cfd0c22a930179ed3fb63e
Myan <r.k.dubowsky@gmail.com>
2015-07-17 12:25:28 -0400

    Re-wrote query file to run at a reasonable speed. 3yr query from ~90 seconds to ~15. Did not re-write map to work with new object -- should be trivial

--- .tmp/public/js/src/countyChorQuery.js
+++ .tmp/public/js/src/countyChorQuery.js
@@ -2,7 +2,7 @@ function countyChorQuery(params,callback){
 
 var url = "/storms/countyChorMap/"+params.startYear+"/"+params.endYear;
 console.log("URL in clorQuery",url);
-
+console.time("first");
 eventTypeArray=["Hail","Thunderstorm Wind","Flood","Flash Flood","Lightning","Heavy Rain","Tornado","All"];
 
 //Scale to make numbers look normal
@@ -46,19 +46,61 @@ d3.json(url,function(err,dataFromServer){
         //console.log("new counties",demo);
 
 
+        var theObj = {}
+        //I want an object where key = id + type
         demo.features.forEach(function(item){
                 eventTypeArray.forEach(function(eType){
-                    stateEventSumArray.push({id:item.id,type:eType,damage:0});               
+
+                    theObj[item.id + eType] = {damage:0};
+                    // stateEventSumArray.push(theObj);               
                 })
         })
-
+        //console.log(theObj);
 
             mapData = dataFromServer.state.rows;
 
             ///Filter out null data
-            locationEvents = mapData.filter(function(event){
+            stateEventSumArray = mapData.filter(function(event){
+                //Filters out new rows, passes it all to map function
                 return event.DAMAGE_PROPERTY != null && event.DAMAGE_PROPERTY != "0.00K";
-            })
+            }).map(function(event){
+                //Map function creates the statecounty thing and new object, sends it to the last map
+                if(event.CZ_FIPS.length < 3){
+                    event.CZ_FIPS = "0" + event.CZ_FIPS
+                }
+                return{
+                    STATECOUNTY: stateFips[event.STATE] + event.CZ_FIPS,
+                    DAMAGE_PROPERTY: +damagePopScale(event.DAMAGE_PROPERTY),
+                    EVENT_TYPE: event.EVENT_TYPE
+                };
+            }).reduce(function(newDataObject,curEvent){
+                //Want to reduce from 1 event per year per type county
+                //To 1 event per type county
+
+                //If current event countyType = newDataObject[countyType]
+                //Add damages
+                var curCountyEvent = curEvent.STATECOUNTY + curEvent.EVENT_TYPE;
+                //console.log(curCountyEvent)
+                if(theObj[curCountyEvent] != null){
+                    //console.log("we made it");
+                    if(!isNaN(curEvent.DAMAGE_PROPERTY)){
+                        theObj[curCountyEvent].damage = +theObj[curCountyEvent].damage + +curEvent.DAMAGE_PROPERTY
+                    }
+                }
+
+                var curAll = curEvent.STATECOUNTY + "All"
+                if(theObj[curAll] != null){
+                    if(!isNaN(curEvent.DAMAGE_PROPERTY)){
+                        theObj[curAll].damage = +theObj[curAll].damage + +curEvent.DAMAGE_PROPERTY
+                    }
+                }
+
+            },theObj)
+
+console.log(theObj);
+
+//I have X number items for each state/county/event
+//I want to reduce 
 
 
             //Now, need to GROUP BY STATE/COUNTY/EVENT
@@ -67,38 +109,29 @@ d3.json(url,function(err,dataFromServer){
 
             //Make the damage number usable
             //Change from named state to state fips
-            //add STATE/COUNTY attribute
-            locationEvents.forEach(function(event){
-                event.DAMAGE_PROPERTY = +damagePopScale(event.DAMAGE_PROPERTY);
-                event.STATE = stateFips[event.STATE];
+            // //add STATE/COUNTY attribute
+            // locationEvents.forEach(function(event){
+        
 
-                //Add leading zeroes where needed
-                if(event.CZ_FIPS.length < 3){
-                    event.CZ_FIPS = "0" + event.CZ_FIPS
-                }
-                //merge STATE/COUNTY                
-                event.STATECOUNTY = event.STATE + event.CZ_FIPS;
-            })
+            // //stateEventSumArray has 1 row per STATECOUNTY and EVENT_TYPE combo
+            // //So if we go through each row of data, if STATECOUNTY and EVENT_TYPE match, push it
+            //     stateEventSumArray.forEach(function(stateCountyEvent){
+            //         if(event.STATECOUNTY == stateCountyEvent.id && event.EVENT_TYPE == stateCountyEvent.type){
+            //             if(!isNaN(event.DAMAGE_PROPERTY)){
+            //                 stateCountyEvent.damage = +event.DAMAGE_PROPERTY
+            //             }
+            //         }
+            //         if(event.STATECOUNTY == stateCountyEvent.id && stateCountyEvent.type == "All"){
+            //             if(!isNaN(event.DAMAGE_PROPERTY)){
+            //                 stateCountyEvent.damage = stateCountyEvent.damage + +event.DAMAGE_PROPERTY
+            //             }
+            //         }
+            //     })
 
 
-            //stateEventSumArray has 1 row per STATECOUNTY and EVENT_TYPE combo
-            //So if we go through each row of data, if STATECOUNTY and EVENT_TYPE match, push it
 
+            // })
 
-            locationEvents.forEach(function(event){
-                stateEventSumArray.forEach(function(stateCountyEvent){
-                    if(event.STATECOUNTY == stateCountyEvent.id && event.EVENT_TYPE == stateCountyEvent.type){
-                        if(!isNaN(event.DAMAGE_PROPERTY)){
-                            stateCountyEvent.damage = +event.DAMAGE_PROPERTY
-                        }
-                    }
-                    if(event.STATECOUNTY == stateCountyEvent.id && stateCountyEvent.type == "All"){
-                        if(!isNaN(event.DAMAGE_PROPERTY)){
-                            stateCountyEvent.damage = stateCountyEvent.damage + +event.DAMAGE_PROPERTY
-                        }
-                    }
-                })
-            })
 
 
 
@@ -110,9 +143,9 @@ d3.json(url,function(err,dataFromServer){
 
 
 
+            console.timeEnd("first");
 
-
-        	callback(stateEventSumArray)
+        	//callback(stateEventSumArray)
         });// end d3.json
     })
 
